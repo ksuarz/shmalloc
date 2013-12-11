695913b3d1a3ca71d546a6073c6f9ad9332c4773
katiepru <katiepru@nbcs.rutgers.edu>
2013-12-11 18:29:28 -0500

    more tests

--- tests-basic.c
+++ tests-basic.c
@@ -47,6 +47,29 @@ int main(int argc, char *argv[])
     printf("Freeing ptr0\n");
     shmfree(ptr[0], MEM_SIZE);
 
+    //Allocate super big thing
+    printf("Allocating more than we have\n");
+    size = MEM_SIZE - sizeof(Header) + 10;
+    ptr[0] = shmalloc(2, &size, mem, MEM_SIZE);
+    if(ptr[0] != NULL)
+    {
+        fprintf(stderr, "Failed test to allocate more than we have.\n");
+        return 1;
+    }
+
+    //Two references
+    printf("Testing multiple references\n");
+    size = sizeof(int);
+    ptr[0] = shmalloc(9, &size, mem, MEM_SIZE);
+    *((int *)ptr[0]) = 5;
+
+    ptr[1] = shmalloc(9, &size, mem, MEM_SIZE);
+
+    if(*((int *)ptr[1]) != 5)
+    {
+        fprintf(stderr, "Multiple refernces did not work. Got %d, expecting 5\n", *((int *)ptr[1]));
+        return 1;
+    }
 
 
 
