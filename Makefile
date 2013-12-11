3434433955fa1fb17b9cc89624103cdd2579edd0
katiepru <katiepru@nbcs.rutgers.edu>
2013-12-11 17:40:13 -0500

    adding some basic tests

--- Makefile
+++ Makefile
@@ -2,10 +2,13 @@ CC = gcc
 CFLAGS = -ggdb -Wall -pthread
 LATEX = pdflatex
 
-all : shmalloc.o tests
+all : shmalloc.o tests tests-basic
 
 tests: tests.o shmalloc.o
 	$(CC) $(CFLAGS) -o tests shmalloc.o tests.o
+
+tests-basic: tests-basic.o shmalloc.o
+	$(CC) $(CFLAGS) -o tests-basic shmalloc.o tests-basic.o
 
 shmalloc.o: shmalloc.c shmalloc.h
 	$(CC) $(CFLAGS) -c shmalloc.c
@@ -13,10 +16,13 @@ shmalloc.o: shmalloc.c shmalloc.h
 tests.o: tests.c
 	$(CC) $(CFLAGS) -c tests.c
 
+tests-basic.o: tests-basic.c
+	$(CC) $(CFLAGS) -c tests-basic.c
+
 readme: README.ltx
 	$(LATEX) README.ltx 2>&1
 
 clean :
 	@rm -f *.o *.a
-	@rm -f tests
+	@rm -f tests tests-basic
 	@rm -f README.log README.dvi README.aux
---
+++ tests-basic.c
@@ -0,0 +1,53 @@
+#include "shmalloc.h"
+#include <stdio.h>
+#include <string.h>
+
+#define MEM_SIZE 5000
+
+int main(int argc, char *argv[])
+{
+    char mem[MEM_SIZE];
+    size_t size = 10;
+    void *ptr[10];
+
+    memset(mem, 0, MEM_SIZE);
+
+    //Basic usage of malloc
+    printf("Allocating ptr0\n");
+    ptr[0] = shmalloc(1, &size, mem, MEM_SIZE);
+    printf("Freeing ptr0\n");
+    shmfree(ptr[0], MEM_SIZE);
+
+    //Allocate 2 things
+    printf("Allocating ptr0\n");
+    ptr[0] = shmalloc(1, &size, mem, MEM_SIZE);
+    printf("Allocating ptr1\n");
+    ptr[1] = shmalloc(1, &size, mem, MEM_SIZE);
+    //Checking that size is right
+    if(size != 10)
+    {
+        fprintf(stderr, "Size returned from shmalloc is %lu, expected 10\n", (unsigned long) size);
+        return 1;
+    }
+    printf("Freeing ptr0\n");
+    shmfree(ptr[0], MEM_SIZE);
+    printf("Freeing ptr1\n");
+    shmfree(ptr[1], MEM_SIZE);
+
+    //Free something twice
+    printf("Attempting to free ptr0 again\n");
+    shmfree(ptr[0], MEM_SIZE);
+
+    //Free ptr not returned by malloc
+    printf("Allocating ptr0\n");
+    ptr[0] = shmalloc(1, &size, mem, MEM_SIZE);
+    printf("Freeing invalid ptr\n");
+    shmfree(((char *) ptr[0] + 1), MEM_SIZE);
+    printf("Freeing ptr0\n");
+    shmfree(ptr[0], MEM_SIZE);
+
+
+
+
+    return 0;
+}
